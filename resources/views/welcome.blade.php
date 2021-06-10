4fc4ccf4b007cb1bbd8ba67e3a08f4f1dd0da9b9
AlwiMiftahulKaromi <alwimiftahulkaromi@gmail.com>
2021-06-11 01:47:02 +0700

    hiddenlogin buat admindokter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,31 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     </head>
+    
 
+    <style>
+    .mydivouter{
+      position:relative;
+      background: rgba(0, 0, 0, 0.0);
+        width: 200px;
+        height: 76px;
+        margin: 0 auto;
+    }
+    .mydivoverlap{
+        position: relative;
+        z-index: 1;
+    }
+    .mybuttonoverlap{
+      position: relative;
+        z-index: 2;
+        top: 0px;
+        display: none;
+        left: 0px;	
+    }
+    .mydivouter:hover .mybuttonoverlap{ 
+      display:block;
+    }
+    </style>
     <body style = "background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(./images/LOGIN.png);
     background-size: cover;">
         <section>
@@ -20,6 +44,13 @@
               <form action="{{url('login-mahasiswa')}}">
                 <button type="submit">Login</button>
               </form>
+              
+              <div class="mydivouter">	
+                <a href="{{url('login/admin')}}" class="mybuttonoverlap btn btn-info">Login Admin</a>
+                <a href="{{url('login/dokter')}}" class="mybuttonoverlap btn btn-info">Login Dokter</a>
+              </div>
+              
+              
             </div>
 
         </section>
